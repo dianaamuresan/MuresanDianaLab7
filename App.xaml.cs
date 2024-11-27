fc6622beb2331ebcbbf46757c8630de8209b3ac3
Diana-Beatrice Muresan <diana.muresan116@gmail.com>
2024-11-27 18:21:49 +0200

    Am creat un folder Models și o clasă numită ShopList Am adăugat pachetul sqlite-net-pcl și am configurat baza de date locală în clasa ShoppingListDatabase Am implementat paginile ListEntryPage(pentru afișarea și selectarea listelor de cumpărături) și ListPage(pentru editarea și ștergerea listelor) Am adăugat ListEntryPage în AppShell.xaml pentru navigare prin tab-uri.

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,12 +1,30 @@
-﻿namespace MuresanDianaLab7
+﻿using System;
+using MuresanDianaLab7.Data;
+using System.IO;
+
+namespace MuresanDianaLab7
 {
     public partial class App : Application
     {
+        static ShoppingListDatabase database;
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new
+                   ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.
+                   LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
         public App()
         {
             InitializeComponent();
-
             MainPage = new AppShell();
         }
+
     }
 }
